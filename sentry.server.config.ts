c63e2db584b8ac7b1b576a4bacd3149e9bd01908
7rohitk <166981199+7rohitk@users.noreply.github.com>
2026-01-21 21:55:10 +0530

    Update Sentry DSN in configuration file

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -5,7 +5,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://1964c0b74e587189c8fbe41627de0a79@o4506876178464768.ingest.us.sentry.io/4509366354247680",
+   dsn: "https://74ca99f048f92a8227b688b93b700bc1@o4510748983427072.ingest.us.sentry.io/4510749247471616",
 
   // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
